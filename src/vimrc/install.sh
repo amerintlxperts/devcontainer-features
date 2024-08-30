93c3f638c753f660372e99dc8a49e5494e450b93
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-08-30 09:57:23 -0400

    updating presentation

--- src/vimrc/install.sh
+++ src/vimrc/install.sh
@@ -24,4 +24,4 @@ su -l "${_REMOTE_USER}" -c "git clone https://github.com/vim-airline/vim-airline
 su -l "${_REMOTE_USER}" -c "git clone https://github.com/hashivim/vim-terraform.git ${_REMOTE_USER_HOME}/.vim/pack/plugin/start/vim-terraform"
 su -l "${_REMOTE_USER}" -c "mkdir -p ${_REMOTE_USER_HOME}/.vim/pack/themes/start"
 su -l "${_REMOTE_USER}" -c "git clone https://github.com/tomasiser/vim-code-dark ${_REMOTE_USER_HOME}/.vim/pack/themes/start/vim-code-dark"
-su -l "${_REMOTE_USER}" -c "curl -L -o ${_REMOTE_USER_HOME}/.vimrc https://raw.githubusercontent.com/amerintlxperts/dotfiles/main/.vimrc"
+su -l "${_REMOTE_USER}" -c "curl -L -o ${_REMOTE_USER_HOME}/.vimrc https://raw.githubusercontent.com/robinmordasiewicz/dotfiles/main/.vimrc"
---
+++ src/yq/README.md
@@ -0,0 +1,20 @@
+
+# yq (yq)
+
+Install yq
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/robinmordasiewicz/features/yq:0": {}
+}
+```
+
+
+
+
+
+---
+
+_Note: This file was auto-generated from the [devcontainer-feature.json](https://github.com/robinmordasiewicz/features/blob/main/src/yq/devcontainer-feature.json).  Add additional notes to a `NOTES.md`._
