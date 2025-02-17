55b918a11ee4ac1355569bf9015047ab1c7360ca
Robin Mordasiewicz <robin@mordasiewicz.com>
2025-02-16 21:46:57 -0500

    adding lacework cli

--- src/lacework-cli/install.sh
+++ src/lacework-cli/install.sh
@@ -22,28 +22,7 @@ create_cache_dir() {
   fi
 }
 
-create_symlink_file() {
-  local local_file="${1}"
-  local cache_file="${2}"
-  local username="${3}"
-
-  # Ensure the parent directory for the symlink exists.
-  runuser -u "${username}" -- mkdir -p "$(dirname "${local_file}")"
-
-  # If a file (or symlink) already exists, move it aside.
-  if [ -e "${local_file}" ]; then
-    echo "Moving existing ${local_file} to ${local_file}-old"
-    mv "${local_file}" "${local_file}-old"
-  fi
-
-  echo "Symlink ${local_file} to ${cache_file} for ${username}..."
-  runuser -u "${username}" -- ln -s "${cache_file}" "${local_file}"
-}
-
 # Create cache directory for Lacework CLI configuration.
 create_cache_dir "/dc/lacework-cli" "${USERNAME}"
-
-# Create the file that will serve as the target of the symlink.
-touch "/dc/lacework-cli/.lacework.toml"
 
 echo "Finished installing ${FEATURE_ID}"
