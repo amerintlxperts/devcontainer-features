af7d14ef8fb4850c753547cec50779fe9b67a00e
Robin Mordasiewicz <robin@mordasiewicz.com>
2025-02-16 21:59:56 -0500

    adding lacework cli

--- src/lacework-cli/install.sh
+++ src/lacework-cli/install.sh
@@ -6,23 +6,18 @@ curl https://raw.githubusercontent.com/lacework/go-sdk/main/cli/install.sh | bas
 USERNAME="${USERNAME:-${_REMOTE_USER}}"
 FEATURE_ID="lacework-cli"
 
-create_cache_dir() {
-  if [ -d "${1}" ]; then
-    echo "Cache directory ${1} already exists. Skip creation..."
-  else
-    echo "Create cache directory ${1}..."
-    mkdir -p "${1}"
-  fi
+if [ -d "/dc/lacework-cli" ]; then
+  echo "Cache directory /dc/lacework-cli already exists. Skip creation..."
+else
+  echo "Create cache directory /dc/lacework-cli..."
+  mkdir -p "/dc/lacework-cli"
+fi
 
-  if [ -n "${2}" ]; then
-    echo "Change owner of ${1} to ${2}..."
-    chown -R "${2}:${2}" "${1}"
-  else
-    echo "No username provided. Skip chown..."
-  fi
-}
-
-# Create cache directory for Lacework CLI configuration.
-create_cache_dir "/dc/lacework-cli" "${USERNAME}"
+if [ -n "${USERNAME}" ]; then
+  echo "Change owner of /dc/lacework-cli to ${USERNAME}..."
+  chown -R "${USERNAME}:${USERNAME}" "/dc/lacework-cli"
+else
+  echo "No username provided. Skip chown..."
+fi
 
 echo "Finished installing ${FEATURE_ID}"
