dd40e43f89e8e673f4566c865a39a90357a587d9
Robin Mordasiewicz <robin@mordasiewicz.com>
2025-02-17 04:50:59 -0500

    adding lacework cli

--- src/lacework-cli/oncreate.sh
+++ src/lacework-cli/oncreate.sh
@@ -7,3 +7,13 @@ if [ "$(id -u)" != "0" ]; then
   echo "Running oncreate.sh for user ${USER}"
   sudo chown -R "${USER}:${USER}" /dc/lacework-cli
 fi
+
+lacework completion zsh >${HOME}/.oh-my-zsh/completions/_lacework
+
+if [ -f ${HOME}/.lacework.toml ]; then
+  mv ${HOME}/.lacework.toml /dc/lacework-cli/.lacework.toml
+else
+  touch /dc/lacework-cli/.lacework.toml
+fi
+
+ln -sf /dc/lacework-cli/.lacework.toml ${HOME}/.lacework.toml
