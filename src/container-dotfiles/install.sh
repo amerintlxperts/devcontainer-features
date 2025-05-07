f7d0e843a29940505b15e75a04a6956543d36a4e
Robin Mordasiewicz <robin@mordasiewicz.com>
2025-05-07 17:24:03 -0400

    update

--- src/container-dotfiles/install.sh
+++ src/container-dotfiles/install.sh
@@ -11,7 +11,7 @@ check_packages() {
 
 export DEBIAN_FRONTEND=noninteractive
 
-# check_packages locales locales-all
+check_packages locales locales-all
 
 su -l "${_REMOTE_USER}" -c "echo '+nostats +nocomments +nocmd +noquestion +recurse +search' > ${_REMOTE_USER_HOME}/.digrc"
 
@@ -22,23 +22,26 @@ su -l "${_REMOTE_USER}" -c "echo 'eval \"\$(oh-my-posh init bash --config /usr/l
 
 su -l "${_REMOTE_USER}" -c "mkdir ~/.kube"
 
-if command -v az &>/dev/null; then
-  su -l "${_REMOTE_USER}" -c "yes y | az config set auto-upgrade.enable=yes"
-  su -l "${_REMOTE_USER}" -c "yes y | az config set auto-upgrade.prompt=no"
-  #su -l "${_REMOTE_USER}" -c "az provider register --namespace Microsoft.Kubernetes"
-  #su -l "${_REMOTE_USER}" -c "az provider register --namespace Microsoft.ContainerService"
-  #su -l "${_REMOTE_USER}" -c "az provider register --namespace Microsoft.KubernetesConfiguration"
-  su -l "${_REMOTE_USER}" -c "az extension add -n k8s-configuration"
-  su -l "${_REMOTE_USER}" -c "az extension add -n k8s-extension"
-  su -l "${_REMOTE_USER}" -c "az extension add -n aks-preview"
+#if command -v az &>/dev/null; then
+#  su -l "${_REMOTE_USER}" -c "yes y | az config set auto-upgrade.enable=yes"
+#  su -l "${_REMOTE_USER}" -c "yes y | az config set auto-upgrade.prompt=no"
+#  #su -l "${_REMOTE_USER}" -c "az provider register --namespace Microsoft.Kubernetes"
+#  #su -l "${_REMOTE_USER}" -c "az provider register --namespace Microsoft.ContainerService"
+#  #su -l "${_REMOTE_USER}" -c "az provider register --namespace Microsoft.KubernetesConfiguration"
+#  su -l "${_REMOTE_USER}" -c "az extension add -n k8s-configuration"
+#  su -l "${_REMOTE_USER}" -c "az extension add -n k8s-extension"
+#  su -l "${_REMOTE_USER}" -c "az extension add -n aks-preview"
+#
+#  su -l "${_REMOTE_USER}" -c "az config set extension.use_dynamic_install=yes_without_prompt"
+#
+#  if [ ! -d "${_REMOTE_USER_HOME}/.oh-my-zsh/custom" ]; then
+#    su -l "${_REMOTE_USER}" -c "mkdir -p ${_REMOTE_USER_HOME}/.oh-my-zsh/custom && chown $_REMOTE_USER:${_REMOTE_USER} ${_REMOTE_USER_HOME}/.oh-my-zsh/custom"
+#  fi
+#  su -l "${_REMOTE_USER}" -c "curl -L -o ${_REMOTE_USER_HOME}/.oh-my-zsh/custom/az.zsh https://raw.githubusercontent.com/Azure/azure-cli/dev/az.completion"
+#fi
 
-  su -l "${_REMOTE_USER}" -c "az config set extension.use_dynamic_install=yes_without_prompt"
+if command -v dotnet &>/dev/null; then
   su -l "${_REMOTE_USER}" -c "dotnet tool install --global Microsoft.CognitiveServices.Speech.CLI"
-
-  if [ ! -d "${_REMOTE_USER_HOME}/.oh-my-zsh/custom" ]; then
-    su -l "${_REMOTE_USER}" -c "mkdir -p ${_REMOTE_USER_HOME}/.oh-my-zsh/custom && chown $_REMOTE_USER:${_REMOTE_USER} ${_REMOTE_USER_HOME}/.oh-my-zsh/custom"
-  fi
-  su -l "${_REMOTE_USER}" -c "curl -L -o ${_REMOTE_USER_HOME}/.oh-my-zsh/custom/az.zsh https://raw.githubusercontent.com/Azure/azure-cli/dev/az.completion"
 fi
 
 if command -v tmux &>/dev/null; then
